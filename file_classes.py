58a8cde6a60a96fcec0d5dfc8fb76f43d1d683e9
Kirill863 <Byblik4310@gmail.com>
2025-01-20 23:27:18 +0300

    добавил класс  AbstractFile

--- file_classes.py
+++ file_classes.py
@@ -1,2 +1,18 @@
 from abc import ABC, abstractmethod
 
+class AbstractFile(ABC):
+
+    @abstractmethod
+    def read(self):
+        """Абстрактный метод для чтения данных из файла."""
+        pass
+
+    @abstractmethod
+    def write(self, data):
+        """Абстрактный метод для записи данных в файл."""
+        pass
+
+    @abstractmethod
+    def append(self, data):
+        """Абстрактный метод для добавления данных в файл."""
+        pass
