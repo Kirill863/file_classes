78c05dbb1d3a76c802a47be0c04b4f669086bcdb
Kirill863 <Byblik4310@gmail.com>
2025-01-20 23:30:02 +0300

    добавил класс JsonFile

--- file_classes.py
+++ file_classes.py
@@ -1,3 +1,4 @@
+import json
 from abc import ABC, abstractmethod
 
 class AbstractFile(ABC):
@@ -16,3 +17,29 @@ class AbstractFile(ABC):
     def append(self, data):
         """Абстрактный метод для добавления данных в файл."""
         pass
+
+class JsonFile(AbstractFile):
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read(self):
+        with open(self.file_path, 'r') as file:
+            return json.load(file)
+
+    def write(self, data):
+        with open(self.file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+
+    def append(self, data):
+        try:
+            existing_data = self.read()
+        except (FileNotFoundError, json.JSONDecodeError):
+            existing_data = []
+
+        if isinstance(existing_data, list):
+            existing_data.append(data)
+        else:
+            raise ValueError("Existing data is not a list")
+
+        self.write(existing_data)
